74454bacdae52c9abaf7e8d19fc0e1d0cc0f5578
vishal0714 <vishalschaudhari001@gmail.com>
2025-07-06 11:38:45 +0530

    added files and functionality

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-// document
-//   .querySelectorAll('.input-group input[type="radio"]')
-//   .forEach((radio) => {
-//     radio.addEventListener("change", () => {
-//       document.querySelectorAll(".details").forEach((detail) => {
-//         detail.style.display = "none";
-//       });
-//       if (radio.checked) {
-//         const parent = radio.closest(".input-group");
-//         const detail = parent.querySelector(".details");
-//         if (detail) {
-//           detail.style.display = "block";
-//         }
-//       }
-//     });
-//   });
-
-document.querySelectorAll(".input-radio").forEach((radio) => {
-  radio.addEventListener("change", () => {
-    document.querySelectorAll(".card").forEach((card) => {
-      card.classList.remove("active");
+document
+  .querySelectorAll('.input-group input[type="radio"]')
+  .forEach((radio) => {
+    radio.addEventListener("change", () => {
+      document.querySelectorAll(".details").forEach((detail) => {
+        detail.style.display = "none";
+      });
+      if (radio.checked) {
+        const parent = radio.closest(".input-group");
+        const detail = parent.querySelector(".details");
+        if (detail) {
+          detail.style.display = "flex";
+        }
+        const text = parent.parentElement.querySelector(".price h4").innerText;
+        document.getElementById("total").innerText = text;
+      }
     });
-    const card = radio.closest(".card");
-    card.classList.add("active");
   });
-});
